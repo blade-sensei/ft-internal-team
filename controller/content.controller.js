442cd653180a07cbc393973f65a97d60ce3d9727
Coyla <jcoyla123@gmail.com>
2020-02-25 17:33:02 +0100

    chore(ui): add byline and date to content page
    create function to get write format of date: month uppercase date year

--- controller/content.controller.js
+++ controller/content.controller.js
@@ -6,8 +6,19 @@ const contentModel = require('../models/content.model');
 
 router.get('', async (req, res) => {
     const article = contentModel.getArticle();
-    
+    article.publishedData = getDataFormat(article.publishedDate);
+    article.byline = article.byline.substring(3);
     res.render('content.view.html', {article})
 });
+
+function getDataFormat(dateString) {
+    const dateFormat = new Date(dateString);
+    const formatter = new Intl.DateTimeFormat('en-GB', { month: 'long'});
+    const month = formatter.format(dateFormat).toUpperCase();
+    const year = dateFormat.getFullYear();
+    const date = dateFormat.getDate();
+    const fullDateFormat = ` ${month} ${date} ${year}`;
+    return fullDateFormat;
+}
 
 module.exports = router;
