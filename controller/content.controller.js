1e1b64d177187ce9267924c81df7ca8b131a4842
Coyla <jcoyla123@gmail.com>
2020-02-25 01:26:34 +0100

    chore: add content model, this version is mocking data
    data from https://developer.ft.com/portal/docs-start-explore-additional-ft-api-endpoints#Content

--- controller/content.controller.js
+++ controller/content.controller.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
+const contentModel = require('../models/content.model');
 
 
 router.get('', async (req, res) => {
+    const article = contentModel.getArticle();
     res.send('content page');
 });
 
---
+++ models/content.model.js
@@ -0,0 +1,9 @@
+const content = require('./content.mock.json');
+
+function getArticle() {
+    return content;
+}
+
+module.exports = {
+    getArticle,
+}
