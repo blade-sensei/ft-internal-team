cfde358b1fb3be3c6ca7306c815572e02ae81e40
Coyla <jcoyla123@gmail.com>
2020-02-25 22:53:50 +0100

    chore: add function to request FT API and display in html template
    install axios to do http request

--- controller/search.controller.js
+++ controller/search.controller.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
-const contentModel = require('../models/content.model');
+const searchModel = require('../models/search.model');
 
 
 router.get('/', async (req, res) => {
     const queryKeyWord = req.query.q;
-    res.send('render search');
+    const searchResults = await searchModel.searchByKeyWord(queryKeyWord);
+    console.log(searchResults);
+    res.render('search-list.view.html', { results: searchResults });
 });
 
 module.exports = router;
---
+++ models/search.model.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const config = require('../config.json');
+
+async function searchByKeyWord(keywords, options) {
+    const headers = {
+        'X-Api-Key': config.token,
+        'Content-Type': 'application/json'
+    };
+    const body = {
+        queryString: "Donald AND usa",
+        resultContext : {
+            aspects :['images','lifecycle','location','master','nature','summary','title']
+       }
+    };
+    const url = 'http://api.ft.com/content/search/v1';
+    const searchResults = await axios.post(url, body, { headers})
+    return searchResults.data.results[0].results;
+}
+
+module.exports = {
+    searchByKeyWord,
+}
