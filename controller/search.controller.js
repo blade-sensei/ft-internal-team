0530cd645d1439da02f055df2f484a7f265ac5dd
Coyla <jcoyla123@gmail.com>
2020-02-26 01:26:21 +0100

    chore: add date for each article in the research page

--- controller/search.controller.js
+++ controller/search.controller.js
@@ -2,13 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 const searchModel = require('../models/search.model');
+const utils = require('../helpers/utils');
+
 
 
 router.get('/', async (req, res) => {
     const queryKeyWord = req.query.q;
-    const searchResults = await searchModel.searchByKeyWord(queryKeyWord);
-    console.log(searchResults);
+    let searchResults = await searchModel.searchByKeyWord(queryKeyWord);
+    searchResults = injectFormatedData(searchResults);
     res.render('search-list.view.html', { results: searchResults });
 });
+
+function injectFormatedData(articles) {
+    return articles.map(article => {
+        article.date = utils.getDataFormat(article.lifecycle.lastPublishDateTime);
+        return article;
+    })
+}
 
 module.exports = router;
