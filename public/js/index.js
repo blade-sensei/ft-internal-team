fdf9fae67a22bb49d44d82ae882705cf10c521ec
Coyla <jcoyla123@gmail.com>
2020-02-26 16:04:33 +0100

    fix: searchbox could not get the input user for his own form

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
-const searchBtn = document.querySelector('#search-btn-id');
-searchBtn.addEventListener('click', () => {
-    const searchInput = document.querySelector('#search-input-id');
-    const searchText = searchInput.value;
-    let searchKeyWords = searchText.split(' ');
-    searchKeyWords = searchKeyWords.join('+');
-    window.location.href = `http://localhost:3000/search?q=` + searchKeyWords; 
-})
+const searchBtns = document.querySelectorAll('.search-btn');
+setClickHandlerSearchButton(searchBtns);
+
 
 const searchFormDisplayStatus = {
 
@@ -20,4 +15,21 @@ searchBtnToggle.addEventListener('click', () => {
 
 function getToggleDisplay(currentDisplay) {
     return currentDisplay === 'none' ? 'block': 'none'
+}
+
+function setClickHandlerSearchButton(buttons) {
+    for (button of buttons) {
+        setHandleSearchButton(button);
+    }
+}
+
+function setHandleSearchButton(searchBtn) {
+    searchBtn.addEventListener('click', () => {
+        const parent = searchBtn.parentElement;
+        const searchInput = parent.querySelector('.search-input');
+        const searchText = searchInput.value;
+        let searchKeyWords = searchText.split(' ');
+        searchKeyWords = searchKeyWords.join('+');
+        window.location.href = `http://localhost:3000/search?q=` + searchKeyWords; 
+    })
 }
